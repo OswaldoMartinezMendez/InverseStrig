0224ed387fbf82554e4ab670bf848a0036ca28ae
oswaldo martinez <oswaldo1088@gmail.com>
2016-08-13 13:08:24 -0500

    Add class StringHelper

---
+++ InvertString/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvertString
+{
+    public class StringHelper
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
