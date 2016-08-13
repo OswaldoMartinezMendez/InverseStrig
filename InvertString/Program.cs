816916d9ad8054d0ba09957800e91440ceb1d7ad
oswaldo martinez <oswaldo1088@gmail.com>
2016-08-13 13:19:02 -0500

    Add logic into main method on class program.cs

--- InvertString/Program.cs
+++ InvertString/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 
 namespace InvertString
@@ -9,6 +10,14 @@ namespace InvertString
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter input string:");
+            string line = Console.ReadLine();
+            if (line != null && line.Equals("exit"))
+            {
+                Environment.Exit(0);
+            }
+            Console.WriteLine(StringHelper.ReverseString(line));
+            Console.ReadLine();
         }
     }
 }
